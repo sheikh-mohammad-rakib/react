5490584443fda5aa91ab22a596011fb29af75e0a
Sheikh Mohammad Rakib <u2009023@student.cuet.ac.bd>
2025-10-31 22:25:12 +0600

    Refactors app to use a simple button
    Simplifies the application by introducing a basic button component.
    
    This change removes the previous state management logic and
    replaces it with a simple, stateless button. The update streamlines
    the code and prepares the application for future enhancements.

--- src/App.jsx
+++ src/App.jsx
@@ -1,67 +1,16 @@
-import { useState } from 'react';
-
-
-
-const user = {
-  name: 'Hedy Lamarr',
-  imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
-  imageSize: 90,
-};
-
-const products = [
-  { title: 'Cabbage', id: 1 },
-  { title: 'Garlic', id: 2 },
-  { title: 'Apple', id: 3 },
-];
-
-const listItems = products.map(product =>
-  <li
-      key={product.id}
-      style={{
-        color: product.isFruit ? 'magenta' : 'darkgreen'
-      }}
-    >
-      {product.title}
-    </li>
-);
-
-//The export default keywords specify the main component in the file.
-export default function MyApp() { 
-  const [count, setCount] = useState(0);
-
-  function handleClick() {
-    setCount(count + 1);
-  } 
+function MyButton() {
   return (
-    <div>
-      <h1>Welcome to my app</h1>
-      <h1>{user.name}</h1>
-      <img
-        className="avatar"
-        src={user.imageUrl}
-        alt={'Photo of ' + user.name}
-        style={{
-          width: user.imageSize,
-          height: user.imageSize
-        }}
-      />
-      <br />
-      <MyButton count={count} onClick={handleClick} />
-      <br />
-      <MyButton count={count} onClick={handleClick} />
-      <br />
-      <ul>{listItems}</ul>
-
-    </div>
+    <button>
+      I'm a button
+    </button>
   );
 }
 
-function MyButton({ count, onClick }) {
-  
+export default function MyApp() {
   return (
-    <button onClick={onClick}>
-      Clicked {count} times
-      {console.log('Button clicked', count)}
-    </button>
+    <div>
+      <h1>Welcome to my app</h1>
+      <MyButton />
+    </div>
   );
 }
