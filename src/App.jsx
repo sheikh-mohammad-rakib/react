4e3566ee9e0805d9e63575a1b8f51544e14138e4
Sheikh Mohammad Rakib <u2009023@student.cuet.ac.bd>
2025-10-31 23:47:51 +0600

    Renders a shopping list of products
    Replaces the user profile component with a shopping list.
    
    The shopping list displays a list of products, each with a title and a flag indicating whether it is a fruit. The list items are styled based on whether the product is a fruit or not.

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,22 @@
-const user = {
-  name: 'Hedy Lamarr',
-  imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
-  imageSize: 90,
-};
+const products = [
+  {title: 'Cabbage', isFruit: false, id: 1},
+  {title: 'Garlic', isFruit: false, id: 2},
+  {title: 'Apple', isFruit: true, id: 3},
+];
 
-export default function MyApp() {
+export default function ShoppingList() {
+  const listItems = products.map(product =>
+    <li
+      key={product.id}
+      style={{
+        color: product.isFruit ? 'magenta' : 'darkgreen'
+      }}
+      >
+        {product.title}
+    </li>
+  );
+
   return (
-    <div>
-      <h1>{user.name}</h1>
-      <img 
-        src={user.imageUrl} 
-        alt={'Photo of '+user.name}
-        style={{
-          width: user.imageSize,
-          height: user.imageSize
-        }} 
-      />
-    </div>
+    <ul>{listItems}</ul>
   );
 }
