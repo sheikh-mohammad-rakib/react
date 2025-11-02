e0b29092a36556ebf6c00b5d932f0044ca5ab55b
Sheikh Mohammad Rakib <u2009023@student.cuet.ac.bd>
2025-11-02 20:24:59 +0600

    feat: add Playwright configuration, Navbar component, and example tests
    - Created a Playwright configuration file to set up testing environment with parallel execution and retries.
    - Implemented a Navbar component with user login functionality and dropdown menu.
    - Added example tests for verifying page title and navigation on Playwright's official site.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import User from "./User";
+import Navbar from "./Navbar";
 
 export default function App() {
   const [user, setUser] = useState([]);
@@ -12,10 +13,14 @@ export default function App() {
   console.log(user)
 
   return (
-    <div className="flex gap-2 flex-wrap justify-center">
-      {user.map((user) => (
-        <User key={user.id} userInfo={user}/>
-      ))}
-    </div>
+    <>
+      <Navbar />
+      <div className="flex gap-2 flex-wrap justify-center">
+        {user.map((user) => (
+          <User key={user.id} userInfo={user}/>
+        ))}
+      </div>
+    </>
+
   )
 }
--- src/User.jsx
+++ src/User.jsx
@@ -1,6 +1,10 @@
-import React from 'react'
+
 
 export default function User({ userInfo }) {
+
+  const clickHandlear = (data) => {
+    console.log("Button clicked", data);
+  }
   return (
     <div className="card bg-base-100 w-96 shadow-sm">
   <figure>
@@ -12,7 +16,7 @@ export default function User({ userInfo }) {
     <h2 className="card-title">{userInfo.lastName}</h2>
     <p>{userInfo.email}</p>
     <div className="card-actions justify-end">
-      <button className="btn btn-primary">Buy Now</button>
+      <button onClick={() => clickHandlear(userInfo.lastName)} className="btn btn-primary">Buy Now</button>
     </div>
   </div>
 </div>
